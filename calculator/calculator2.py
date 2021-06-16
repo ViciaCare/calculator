38e5590d918004ec7ad418277a9330c1c6414fef
ViciaCare <pvk.sakha@yandex.ru>
2021-06-16 20:01:03 +0900

    added button

--- calculator/calculator2.py
+++ calculator/calculator2.py
@@ -9,6 +9,19 @@ class MainApp(App):
         main_layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
         self.solution = TextInput(multiline=False, readonly=False, halign="right", font_size=30, input_filter="float")
         main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "C", "+"],
+        ]
+        for row in buttons:
+            h_layout = BoxLayout()
+            for label in row:
+                button = Button(text=label, pos_hint={"center_x": 0.5, "center_y": 0.5})
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
+
         return main_layout
 
 
